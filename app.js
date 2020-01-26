779d57c3cc14fcdd808c8d7bb1d8ba375a9a81fb
Yeimer Molina <yeimer.molina@gmail.com>
2020-01-25 21:24:39 -0500

    Update port

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ mongoose
   )
   .then(() => {
     console.log("Running App on Port 5000");
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(e => {
     console.log(e);
